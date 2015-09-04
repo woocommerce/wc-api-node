e08decd91ef4afe567e36a7bd0149dda0b061686
unknown <kriz@live.com>
2015-09-05 02:08:38 +0300

    set request return explicitly json

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ WooCommerceAPI.prototype._request = function(method, endpoint, data, callback) {
     url: url,
     method: method,
     encoding: this.encoding,
+    json: true,
     headers: {
       'User-Agent': 'WooCommerce API Client-Node.js/' + this.classVersion,
       'Content-Type': 'application/json',
