922fce0b1f3a3e653c4581fd707c6e018c62f836
Claudio Sanches <contato@claudiosmweb.com>
2015-12-07 20:16:46 -0200

    Version 1.0.3

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function WooCommerceAPI(opt) {
     throw new Error('consumerSecret is required');
   }
 
-  this.classVersion = '1.0.2';
+  this.classVersion = '1.0.3';
   this._setDefaultsOptions(opt);
 }
 
